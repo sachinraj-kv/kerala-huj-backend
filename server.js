5318282e1a54e9481f87140882667e19dfaab23c
sachinraj-kv <sachinrajkv22@gmail.com>
2025-08-27 16:34:52 +0530

    Update server.js
    port

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = require('./app/app');
 const mongoDB_Connect = require('./connect/mongoConnect');
 
 mongoDB_Connect()
-app.listen("3000",()=>{
-    console.log("server running on 3000");
+app.listen(process.env.PORT,()=>{
+    console.log(`server running on${process.env.PORT}`);
     
 })
+
 
