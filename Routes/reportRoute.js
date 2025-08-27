3fd195480c1c951b3310ff2d91b30e9d7fa29678
sachinraj-kv <sachinrajkv22@gmail.com>
2025-08-27 14:44:19 +0530

    Update reportRoute.js
    put and get same url changed to deiffernt lines

--- Routes/reportRoute.js
+++ Routes/reportRoute.js
@@ -7,9 +7,11 @@ const { authorization } = require('../middleware/authirization');
 const router = express.Router();
 
 router.route('/submit').post(authorization,submitReport)
-router.route('/view').get(viewReport).put(Update)
+router.get('/view', viewReport);
+router.put('/view', Update);
 router.route('/search').post(reportsearch)
 router.route('/details/:id').get(report)
 
 
 module.exports = router;
+
