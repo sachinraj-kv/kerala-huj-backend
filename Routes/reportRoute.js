41e2b81f34fb9b00886958d573e215ffd2e24533
sachinraj-kv <sachinrajkv22@gmail.com>
2025-08-27 15:33:04 +0530

    Update reportRoute.js

--- Routes/reportRoute.js
+++ Routes/reportRoute.js
@@ -7,13 +7,14 @@ const { authorization } = require('../middleware/authirization');
 const router = express.Router();
 
 router.route('/submit').post(authorization,submitReport)
-router.route('/view') put(viewReport)
-router.route('/update').put (Update)
+router.route('/view').get(viewReport)
+router.route('/update').put(Update)
 router.route('/search').post(reportsearch)
 router.route('/details/:id').get(report)
 
 
 module.exports = router;
+
 
 
 
