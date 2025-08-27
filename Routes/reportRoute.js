ceb3f052e9dbd4413144eb0c976b6ad15f4b67e2
sachinraj-kv <sachinrajkv22@gmail.com>
2025-08-27 15:27:29 +0530

    Update reportRoute.js
    error fixed

--- Routes/reportRoute.js
+++ Routes/reportRoute.js
@@ -7,13 +7,14 @@ const { authorization } = require('../middleware/authirization');
 const router = express.Router();
 
 router.route('/submit').post(authorization,submitReport)
-router.get('/view', viewReport)
-router.put('/update', Update)
+router.route('/view') put(viewReport)
+router.route('/update').put (Update)
 router.route('/search').post(reportsearch)
 router.route('/details/:id').get(report)
 
 
 module.exports = router;
+
 
 
 
