476f02030e50529a18ee3a7d4441988d07220ce9
sachinraj-kv <sachinrajkv22@gmail.com>
2025-08-27 13:16:31 +0530

    Update reportRoute.js
    unexpected import auth

--- Routes/reportRoute.js
+++ Routes/reportRoute.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const { submitReport, viewReport, Update, reportsearch, report } = require('../Controller/reportController');
 const { authorization } = require('../middleware/authirization');
-const { route } = require('./authRoute');
+
 
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router.route('/submit').post(authorization,submitReport)
 router.route('/view').get(viewReport).put(Update)
 router.route('/search').post(reportsearch)
 router.route('/details/:id').get(report)
+
 
 module.exports = router;
